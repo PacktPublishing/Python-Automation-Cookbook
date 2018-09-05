93263dbc179b5ea6141e2e674c64b56a600bb0af
Jaime Buelta <jaime.buelta@gmail.com>
2018-09-05 22:11:57 +0100

    Change script

--- chapter10/debug_skills.py
+++ chapter10/debug_skills.py
@@ -21,11 +21,13 @@ for name in NAMES:
         'custname': name,
     }
     result = requests.post('http://httpbin.org/post', json=data)
+    if result.status_code != 200:
+        raise Exception('Error accessing server')
     raw_result = result.json()['data']
     full_name = parse.search('"custname": "{name}"', raw_result)['name']
     first_name, last_name = full_name.split()
-    name = f'{last_name}, P{first_name}'
-    sorted_names.append(name)
+    ready_name = f'{last_name}, {first_name}'
+    sorted_names.append(ready_name)
 
 sorted_names.sort()
 print(sorted_names)
